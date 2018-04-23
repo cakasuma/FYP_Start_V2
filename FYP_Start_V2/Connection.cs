5ea54c8cb157959830e1004330e646d56840e154
cakasuma <amammustofa@gmail.com>
2018-04-23 19:12:05 +0800

    Foundation FYP user_application

---
+++ FYP_Start_V2/Connection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FYP_Start_V2
+{
+    public class Connection
+    {
+        public static SqlConnection getConnection()
+        {
+            SqlConnection conn = null;
+
+            conn = new SqlConnection(@"Data Source=.;Initial Catalog=FYP_Start;Integrated Security=True");
+            return conn;
+        }
+
+        public static void executeQuery(String strQuery)
+        {
+
+
+            SqlConnection conn = getConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(strQuery, conn);
+            cmd.ExecuteNonQuery();
+            closeConnection(conn);
+
+        }
+
+        public static void closeConnection(SqlConnection conn)
+        {
+
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
---
+++ FYP_Start_V2/Home.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="FYP_Start_V2.Home" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>

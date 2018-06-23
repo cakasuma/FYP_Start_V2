3b1c915c7e2248cd07a52b8e8d806e062d0123bd
cakasuma <amammustofa@gmail.com>
2018-06-24 06:37:25 +0800

    fix minor bugs

--- FYP_Start_V2/File_Type.cs
+++ FYP_Start_V2/File_Type.cs
@@ -10,7 +10,7 @@ namespace FYP_Start_V2
         public string[] Document = { "doc", "docx", "odt", "rtf", "txt", "pdf", "html", "htm", "ppt", "pptx" };
         public string[] Compressed = {"zip", "rar", "iso", "arj", "tar.gz", "tgz" };
         public string[] Application = { "exe", "apk", "dmg", "pkg" };
-        public string[] Image = { "jpg", "jpeg", "tiff", "jfif", "exif", "gif", "bmp", "png", "ppm", "pgm", "pbm", "pnm", "webp", "heif", "bat", "bpg", "cgm", "svg", "ai", "ps" };
+        public string[] Image = { "jpg","JPG", "jpeg", "tiff", "jfif", "exif", "gif", "bmp", "png", "ppm", "pgm", "pbm", "pnm", "webp", "heif", "bat", "bpg", "cgm", "svg", "ai", "ps" };
         public string[] Music = { "mp3", "wav", "aiff", "au", "pcm", "ape", "flac", "wv", "m4a", "opus", "aac", "vorbis", "wma", "atrac" };
         public string[] Video = { "avi", "flv", "wmv", "mov", "mp4", "mkv", "webm", "vob", "ogg", "drc", "yuv", "rm", "asf", "amv", "mp4", "m4v", "svi", "3gp" };
 
